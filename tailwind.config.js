2e2b9c49128d9099d862755623761c057324c7f1
Ronak Seth <120469513+Ronak123Seth@users.noreply.github.com>
2024-10-01 19:38:11 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,css,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bgDark: "#22223b",
+        bgDark2: "#4a4e69",
+      },
+    },
+  },
+  plugins: [],
+};
